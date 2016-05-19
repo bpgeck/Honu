7859ebd36574d534303bfe32531cbd8e012e6f81
bpgeck <b.geck@yahoo.com>
2016-05-18 17:16:11 -0700

    Added comments where speech bubbles should be. CTRL+F "SPEECH BUBBLES"

--- Honu/Assets/Scripts/CharacterInteractionAndInventory.cs
+++ Honu/Assets/Scripts/CharacterInteractionAndInventory.cs
@@ -65,6 +65,9 @@ public class CharacterInteractionAndInventory : MonoBehaviour
                     {
                         Debug.Log("Grabbing Toolbox");
 
+                        // SPEECH BUBBLE
+                        // Text: "Hmmm, maybe this toolbox will come in handy"
+
                         hasToolbox = true;
                         hits[i].collider.gameObject.SetActive(false);
                     }
@@ -74,16 +77,33 @@ public class CharacterInteractionAndInventory : MonoBehaviour
 
                         if (hasToolbox)
                         {
+                            // SPEECH BUBBLE
+                            // Text: "One man's trash is another man's propeller"
+
                             hasPropeller = true;
                             hits[i].collider.gameObject.tag = "Untagged";
+                        }
+                        else
+                        {
+                            // SPEECH BUBBLE
+                            // Text: "Hey there's a broken fan in here. Nothing looks broken. I guess the motor must have gone out."
                         }
                     }
                     else if (hits[i].collider.name.Contains("Driftwood"))
                     {
+                        // SPEECH BUBBLE
+                        // Text: "Driftwood. That might come in handy."
+
                         Debug.Log("Grabbing Driftwood");
 
                         numDriftwood++;
                         hits[i].collider.gameObject.SetActive(false);
+
+                        if (numDriftwood == 5)
+                        {
+                            // SPEECH BUBBLE
+                            // Text: "Alright that's all the driftwood I can carry."
+                        }
                     }
                     else if (hits[i].collider.name.Contains("Closet"))
                     {
@@ -91,14 +111,25 @@ public class CharacterInteractionAndInventory : MonoBehaviour
 
                         if (hasHint && hasBirthday)
                         {
+                            // SPEECH BUBBLE
+                            // Text: "Alright let's try this out... Aha! Scuba gear!"
+
                             hasScubaGear = true;
                             hits[i].collider.gameObject.SetActive(false);
                             GameObject.Find("OpenLockedCloset").GetComponent<SpriteRenderer>().enabled = true;
+                        }
+                        else
+                        {
+                            // SPEECH BUBBLE
+                            // Text: "The storage closet is locked. The last guy here must have set a new code."
                         }
                     }
                     else if (hits[i].collider.name.Contains("Calendar"))
                     {
                         Debug.Log("Reading Calendar");
+
+                        // SPEECH BUBBLE
+                        // Text: ""Jamie's Birthday: 03-18-08." Huh. Happy belated birthday Jamie."
 
                         hasBirthday = true;
                         hits[i].collider.gameObject.tag = "Untagged";
@@ -107,6 +138,12 @@ public class CharacterInteractionAndInventory : MonoBehaviour
                     {
                         Debug.Log("Reading Computer");
 
+                        // SPEECH BUBBLE
+                        // Text: "This looks like the work log for the last scientist:
+                        //       "Day 1: Cataloged plenty of sea turtles today. Good to see they're still migrating on time.
+                        //       "Day 2: I've noticed some of the turtles are getting attracted to the lights of my boat. I'll stop going out at night now.
+                        //       "Day 3: Accidentaly broke the lock on the closet downstairs. Make sure to tell the next scientist the passcode is my daughter's birthday.
+
                         hasHint = true;
                         hits[i].collider.gameObject.tag = "Untagged";
                     }
@@ -114,20 +151,31 @@ public class CharacterInteractionAndInventory : MonoBehaviour
                     {
                         if (hasToolbox && numDriftwood == 5 && !hullFixed)
                         {
+                            // SPEECH BUBBLE
+                            // Text: Alright driftwood plus tools equals a fixed hole. Awesome!
+
                             hullFixed = true;
                         }
                         
                         if (hasToolbox && hasPropeller && !propFixed)
                         {
+                            // SPEECH BUBBLE
+                            // Text: Fan propeller. Boat propeller. Same thing. Fixed!
+
                             propFixed = true;
                         }
 
                         if (hullFixed && propFixed && hasScubaGear)
                         {
-                            // Time to move onto the next zone!
+                            // SPEECH BUBBLE
+                            // Text: Sweet it looks like that's everything. Let's get out on the sea and find that little guy."
                         }
                     }
-                }
+                    else if (hits[i].collider.name.Contains("Ship"))
+                    {
+                        // SPEECH BUBBLE
+                        // Text: "Accoring to my radar, the turtle is in the rocky region of the sea. I should probably stick with a smaller ship today."
+                    }
             }
         }
 	}
